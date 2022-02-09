cfc1c88ac4d6bf03c6230aba19abfd1de904f3f3
Artem Ignatenko <artem.ig1990@gmail.com>
2022-02-09 11:25:45 +0200

    remove unused code

--- smoke_test_assist.py
+++ smoke_test_assist.py
@@ -21,27 +21,5 @@ def create_order(params: dict):
 def place_order(params: dict):
     envi = {}
 
-"""
-def test_create_order_pay():
-    envi = common_functions.prepare_envi()
-    envi.update({"price": 11})
-    envi.update({"status": config.order_status_hew})
-    new_order = common_functions.run_request(general_api_requests.create_order(envi), True)
-    if new_order["code"] == 201:
-        order_data = new_order["body"].get("data")
-        price = order_data["price"]
-    try:
-        assert new_order["code"] == 201 and price != 0
-    except AssertionError as err:
-        print(err)
-    uid = new_order["body"].get("data").get("uid")
-    envi.update({"uid": uid})
-    delete_order = common_functions.run_request(general_api_requests.delete_order(envi), True)
-    try:
-        assert delete_order["code"] == 200
-    except AssertionError as err:
-        print(err)
-    finally:
-        print("done")
-"""
+
 
