8e4489472c21264c8074cf0bb86e358111147643
Artem Ignatenko <artem.ig1990@gmail.com>
2022-02-09 09:08:22 +0200

    create client orders

--- test_run.py
+++ test_run.py
@@ -9,13 +9,11 @@ class MyTestCase(unittest.TestCase):
         assert result.get("status_code") == 201
         assert result["data"].get("price") == 0
 
-
     def test_create_order_pay(self):
         params = {"price": 11, "status": 0, "client_order": False}
         result = smoke_test_assist.create_order(params)
         assert result.get("status_code") == 201
         assert result["data"].get("price") == 11.0
-
 
     def test_create_order_no_pay_book(self):
         params = {"price": 0, "status": 1, "client_order": False}
@@ -23,13 +21,36 @@ class MyTestCase(unittest.TestCase):
         assert result.get("status_code") == 201
         assert result["data"].get("price") == 0
 
-
     def test_create_order_pay_book(self):
         params = {"price": 11, "status": 1, "client_order": False}
         result = smoke_test_assist.create_order(params)
         assert result.get("status_code") == 201
         assert result["data"].get("price") == 11.0
 
+    def test_create_client_order_no_pay(self):
+        params = {"price": 0, "status": 0, "client_order": True}
+        result = smoke_test_assist.create_order(params)
+        assert result.get("status_code") == 201
+        assert result["data"].get("price") == 0
+
+    def test_create_client_order_pay(self):
+        params = {"price": 11, "status": 0, "client_order": True}
+        result = smoke_test_assist.create_order(params)
+        assert result.get("status_code") == 201
+        assert result["data"].get("price") == 11.0
+
+    def test_create_client_order_no_pay_book(self):
+        params = {"price": 0, "status": 1, "client_order": True}
+        result = smoke_test_assist.create_order(params)
+        assert result.get("status_code") == 201
+        assert result["data"].get("price") == 0
+
+
+    def test_create_client_order_pay_book(self):
+        params = {"price": 11, "status": 1, "client_order": True}
+        result = smoke_test_assist.create_order(params)
+        assert result.get("status_code") == 201
+        assert result["data"].get("price") == 11.0
 
 if __name__ == '__main__':
     unittest.main()
